ab1840d352c9275427b6cf51f2d5ffd5859ec86e
craig wann and kyle parsons <kyle@>
2018-06-12 10:57:03 -0700

    add js for addresses

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+//business logic
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.addresses = [];
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("#add-address").click(function(){
+    $("#new-address").append('<div class="new-address">' +
+                              '<div class="form-group">' +
+                                '<label for= "new-street">Street</label>' +
+                                '<input type= "text" class= "form-control new-street">'+
+                              '</div>' +
+                              '<div class= "form-group">' +
+                                '<label for= "new-city">City</label>' +
+                                '<input type= "text" class= "form-control new-city">' +
+                              '</div>' +
+                              '<div class= "form-group">' +
+                                '<label for= "new-state">State</label>' +
+                                '<input type= "text" class= "form-control new-state">' +
+                              '</div>' +
+                            '</div>');
+
+  });
+
+
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
