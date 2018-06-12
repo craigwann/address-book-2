5dbe51ba69ccb6f33ff6748e3b50f025531e83d9
craig wann and kyle parsons <craig@>
2018-06-12 14:28:29 -0700

    final commit remove address working

--- js/scripts2.js
+++ js/scripts2.js
@@ -15,36 +15,10 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-Address.prototype.fullAddress = function() {
-  return this.street + ", " + this.city + ", " + this.state;
-}
-
-function resetFields(){
-  $("input#new-first-name").val("");
-  $("input#new-last-name").val("");
-  $("input.new-street").val("");
-  $("input.new-city").val("");
-  $("input.new-state").val("");
-  $("#new-addresses").empty();
-  $("#new-addresses").append('<div class="new-address">' +
-                               '<div class="form-group">' +
-                                 '<label for="new-street">Street</label>' +
-                                 '<input type="text" class="form-control new-street">' +
-                               '</div>' +
-                               '<div class="form-group">' +
-                                 '<label for="new-city">City</label>' +
-                                 '<input type="text" class="form-control new-city">' +
-                               '</div>' +
-                               '<div class="form-group">' +
-                                 '<label for="new-state">State</label>' +
-                                 '<input type="text" class="form-control new-state">' +
-                               '</div>' +
-                             '</div>');
-}
 // user interface logic
 $(document).ready(function() {
 
-  $("#add-address").click(function(){
+  $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-address">' +
                                  '<div class="form-group">' +
                                    '<label for="new-street">Street</label>' +
@@ -59,9 +33,7 @@ $(document).ready(function() {
                                    '<input type="text" class="form-control new-state">' +
                                  '</div>' +
                                '</div>');
-
   });
-
 
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
@@ -86,12 +58,9 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() +  "</li>");
-      })
-
     });
-    resetFields();
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
   });
 });
